30cd6551cd66a2842deec014de92d81c60c65bfb
Carlo Jude Alfonso <carlojude22@gmail.com>
2017-09-18 17:08:43 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Author: Carlo Jude H. Alfonso
---
+++ assets/js/modules/theme-module.js
@@ -0,0 +1,297 @@
+AppName.Modules.ThemeModule = (function() {
+    //Dependencies
+    var core = AppName.Core;
+
+    //////////////////////
+    // Private Methods //
+    ////////////////////
+
+    var matchHeight = function() {
+        $.fn.equalHeights = function() {
+            var max_height = 0;
+            $(this).each(function() {
+                max_height = Math.max($(this).height(), max_height);
+            });
+            $(this).each(function() {
+                $(this).height(max_height);
+            });
+        };
+        $('.match-height').equalHeights();
+    }
+
+    var owlCarouselBottom = function() {
+        var owl = $('.owl-carousel');
+        if (owl.length) {
+            owl.owlCarousel({
+                navigation: true,
+                slideSpeed: 300,
+                paginationSpeed: 400,
+                items: 3,
+                itemsDesktop: false,
+                itemsDesktopSmall: false,
+                itemsTablet: false,
+                itemsMobile: false,
+                loop: true,
+                autoplay: true,
+                autoplayTimeout: 6000,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    768: {
+                        items: 2
+                    },
+                    992: {
+                        items: 3
+                    }
+                }
+            });
+        }
+        $('.slider-right').click(function() {
+            owl.trigger('next.owl.carousel');
+        })
+        $('.slider-left').click(function() {
+            owl.trigger('prev.owl.carousel', [300]);
+        });
+    }
+
+    var owlCarouselHomeBanner = function() {
+        var bannerMain = $('.banner-main');
+        if (bannerMain.length) {
+            bannerMain.owlCarousel({
+                navigation: true,
+                slideSpeed: 300,
+                paginationSpeed: 400,
+                items: 2,
+                itemsDesktop: false,
+                itemsDesktopSmall: false,
+                itemsTablet: false,
+                itemsMobile: false,
+                loop: true,
+                autoplay: true,
+                autoplayTimeout: 5000
+            });
+        }
+        $('.banner-arrow .slider-right').click(function() {
+            bannerMain.trigger('next.owl.carousel');
+        })
+        $('.banner-arrow .slider-left').click(function() {
+            bannerMain.trigger('prev.owl.carousel', [300]);
+        });
+
+        $('#toggle').click(function() {
+            $(this).toggleClass('active');
+            $('#overlay').toggleClass('open');
+        });
+    };
+
+    var setCookies = function() {
+        $('#closeCookie').click(function(e) {
+            e.preventDefault();
+            $('.cookies').remove();
+            $('procubedServiceCookies', 'ProcubedServiceCookies', { expires: 365 });
+        });
+        if ($('procubedServiceCookies')) {
+            $('.cookies').remove();
+        }
+    }
+
+    var useImage = function() {
+        $('#useImg1').click(function() {
+            $('#drag1').fadeIn();
+            $('#drag1').addClass('zoom');
+            $('.overlay').css({
+                "background-color": "rgba(0, 0, 0, 0.7)"
+            });
+            wheelzoom(document.querySelector('img.zoom'));
+
+            $('.canvas').attr("id", "canvas");
+
+            $('#hideImg1').toggleClass('hidden');
+            $('#useImg1').toggleClass('hidden');
+        });
+
+        $('#hideImg1').click(function() {
+            $('#drag1').fadeOut();
+            $('#drag1').removeClass('zoom');
+            $('.overlay').css({
+                "background-color": "transparent"
+            });
+            document.querySelector('img').dispatchEvent(new CustomEvent('wheelzoom.destroy'));
+
+            $('#useImg1').toggleClass('hidden');
+            $('#hideImg1').toggleClass('hidden');
+        });
+    }
+
+    var closeImg = function() {
+        $('#closeImg').click(function() {
+            $('#drag1').fadeOut();
+            $('#drag1').removeClass('zoom');
+            $('.overlay').css({
+                "background-color": "transparent"
+            });
+            document.querySelector('img').dispatchEvent(new CustomEvent('wheelzoom.destroy'));
+
+            $('.rectangle').remove();
+            $('.circle').remove();
+            $('#useImg1').toggleClass('hidden');
+            $('#hideImg1').toggleClass('hidden');
+        });
+    }
+
+    var removeBox = function() {
+        $('#removeBox').click(function() {
+            $('.rectangle').remove();
+            $('.circle').remove();
+        });
+    }
+
+    var square = function() {
+        $('#square').click(function() {
+            initDraw(document.getElementById('canvas'));
+
+            function initDraw(canvas) {
+                function setMousePosition(e) {
+                    var ev = e || window.event; //Moz || IE
+                    if (ev.pageX) { //Moz
+                        mouse.x = ev.pageX + window.pageXOffset;
+                        mouse.y = ev.pageY + window.pageYOffset;
+                    } else if (ev.clientX) { //IE
+                        mouse.x = ev.clientX + document.body.scrollLeft;
+                        mouse.y = ev.clientY + document.body.scrollTop;
+                    }
+                };
+                var mouse = {
+                    x: 0,
+                    y: 0,
+                    startX: 0,
+                    startY: 0
+                };
+                var element = null;
+
+                canvas.onmousemove = function(e) {
+                    setMousePosition(e);
+                    if (element !== null) {
+                        element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
+                        element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
+                        element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
+                        element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
+                    }
+                }
+
+                canvas.onclick = function(e) {
+                    if (element !== null) {
+                        element = null;
+                        canvas.style.cursor = "crosshair";
+                        console.log("finsihed.");
+                    } else {
+                        console.log("begun.");
+                        mouse.startX = mouse.x;
+                        mouse.startY = mouse.y;
+                        element = document.createElement('div');
+                        element.className = 'rectangle'
+                        element.style.left = mouse.x + 'px';
+                        element.style.top = mouse.y + 'px';
+                        canvas.appendChild(element)
+                        canvas.style.cursor = "crosshair";
+                    }
+                }
+            }
+        });
+    }
+
+    var circle = function() {
+        $('#circle').click(function() {
+            initDraw(document.getElementById('canvas'));
+
+            function initDraw(canvas) {
+                function setMousePosition(e) {
+                    var ev = e || window.event; //Moz || IE
+                    if (ev.pageX) { //Moz
+                        mouse.x = ev.pageX + window.pageXOffset;
+                        mouse.y = ev.pageY + window.pageYOffset;
+                    } else if (ev.clientX) { //IE
+                        mouse.x = ev.clientX + document.body.scrollLeft;
+                        mouse.y = ev.clientY + document.body.scrollTop;
+                    }
+                };
+                var mouse = {
+                    x: 0,
+                    y: 0,
+                    startX: 0,
+                    startY: 0
+                };
+                var element = null;
+
+                canvas.onmousemove = function(e) {
+                    setMousePosition(e);
+                    if (element !== null) {
+                        element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
+                        element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
+                        element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
+                        element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
+                    }
+                }
+
+                canvas.onclick = function(e) {
+                    if (element !== null) {
+                        element = null;
+                        canvas.style.cursor = "crosshair";
+                        console.log("finsihed.");
+                    } else {
+                        console.log("begun.");
+                        mouse.startX = mouse.x;
+                        mouse.startY = mouse.y;
+                        element = document.createElement('div');
+                        element.className = 'circle'
+                        element.style.left = mouse.x + 'px';
+                        element.style.top = mouse.y + 'px';
+                        canvas.appendChild(element)
+                        canvas.style.cursor = "crosshair";
+                    }
+                }
+            }
+        });
+    }
+
+    var popover = function() {
+        $('[data-toggle=popover]').popover();
+    }
+
+    var preloader = function() {
+        $(window).load(function() {
+            $(".se-pre-con").fadeOut('slow');
+        });
+    }
+
+    var screenLoader = function() {
+        setTimeout(function() {
+            $(".se-pre-con").fadeOut('slow');
+        }, 5000);
+    }
+
+    /////////////////////
+    // Public Methods //
+    ///////////////////
+    var init = function() {
+        screenLoader();
+        owlCarouselBottom();
+        useImage();
+        removeBox();
+        square();
+        circle();
+        closeImg();
+        popover();
+    };
+
+    var resize = function() {
+        matchHeight();
+    };
+
+    return {
+        init: init,
+        resize: resize
+    };
+})();
